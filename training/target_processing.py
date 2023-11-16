32b6c5b6d93e5a6c6bbed24461ad7b43ef53e886
juliusw <uimmv@student.kit.edu>
2023-11-16 14:16:58 +0100

    updated requirements

--- training/target_processing.py
+++ training/target_processing.py
@@ -1,9 +1,9 @@
 import warnings
 from ete3 import Tree
 import pandas as pd
+import os
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
-import os
 
 
 def calculate_support_statistics(support_file_path, dataset_name):
@@ -15,53 +15,24 @@ def calculate_support_statistics(support_file_path, dataset_name):
     for node in phylo_tree.traverse():
         branch_id_counter += 1
         if node.support is not None and not node.is_leaf():
-            length = node.dist
             node.__setattr__("name", branch_id_counter)
-
-            number_nodes = sum([1 for node in phylo_tree.traverse()])
-            number_children = sum([1 for node in phylo_tree.traverse() if node.is_leaf()])
-
-            farthest_topo = phylo_tree.get_farthest_leaf(topology_only=True)[1]
-            farthest_branch = phylo_tree.get_farthest_leaf(topology_only=False)[1]
-            length_relative = length / farthest_branch
-            depth = node.get_distance(topology_only=True, target=phylo_tree.get_tree_root())
-            num_children = sum([1 for child in node.traverse()])
-            num_children_inner = sum([1 for child in node.traverse() if not child.is_leaf()])
-            num_children_leaf = sum([1 for child in node.traverse() if child.is_leaf()])
-
-            results.append((dataset_name, node.name, node.support / 100, length, length_relative,depth, depth / farthest_topo, num_children,
-                            num_children / number_nodes, num_children_inner ,num_children_inner / num_children, num_children_leaf, num_children_leaf / number_children,
-                            num_children_inner / num_children_leaf))
-
+            results.append((dataset_name, node.name, node.support / 100))
     return results
 
+if __name__ == '__main__':
 
-loo_selection = pd.read_csv(os.path.join(os.pardir, "data/loo_selection.csv"))
-loo_selection["dataset"] = loo_selection["verbose_name"].str.replace(".phy", "")
+    raw_path = os.path.join(os.path.pardir, "data", "raw")
+    folder_names = [folder for folder in os.listdir(raw_path) if os.path.isdir(os.path.join(raw_path, folder))]
+    print(folder_names)
+    counter = 0
+    results_final = []
+    for file in folder_names:
+        support_path = os.path.join(raw_path, file + "_1000.raxml.support")
+        counter +=1
+        print(f"{counter} / {len(folder_names)}")
+        if os.path.exists(support_path):
+            results_tmp = calculate_support_statistics(support_path, file.replace(".newick", ""))
+            results_final.extend(results_tmp)
 
-already_dedup_bs = pd.read_csv(os.path.join(os.pardir, "data/bs_support_pred_selection.csv"))
-already_dedup_bs["dataset"] = already_dedup_bs["dataset"].str.replace(".newick", "")
-
-loo_selection = loo_selection.merge(already_dedup_bs, on="dataset", how="inner")
-print(loo_selection.shape)
-filenames = loo_selection['verbose_name'].str.replace(".phy", "").tolist()
-results_final = []
-counter = 0
-
-for file in filenames:
-    print(len(filenames))
-    support_path = os.path.join(os.pardir, "scripts/") + file + "_1000.raxml.support"
-    print(support_path)
-    counter +=1
-    print(counter)
-
-    if os.path.exists(support_path):
-        print("Found")
-        results_tmp = calculate_support_statistics(support_path, file.replace(".newick", ""))
-        results_final.extend(results_tmp)
-
-df_final = pd.DataFrame(results_final, columns=["dataset", "branchId", "support", "length", "length_relative", "depth", "depth_relative","num_children",
-                                                "rel_num_children", "num_children_inner", "rel_num_children_inner", "num_children_leaf", "rel_num_children_leaf",
-                                                "child_inner_leaf_ratio"])
-
-df_final.to_csv(os.path.join(os.pardir, "data/processed/target/branch_supports.csv"))
+    df_final = pd.DataFrame(results_final, columns=["dataset", "branchId", "support"])
+    df_final.to_csv(os.path.join(os.pardir, "data/processed/target/branch_supports.csv"))
