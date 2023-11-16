cf68318fcfdd4d31f8d72f3e3722656b3cd6faa2
juliusw <uimmv@student.kit.edu>
2023-11-16 22:51:16 +0100

    refactoring

--- scripts/merge_datasets.py
+++ scripts/merge_datasets.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 features = pd.read_csv( os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "processed", "features.csv"))
-targets = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "processed", "target", "branch_supports.csv")
+targets = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "processed", "target", "branch_supports.csv"))
 
 merged_df = features.merge(targets, how="inner", on=["dataset", "branchId"])
 print(merged_df.shape)
