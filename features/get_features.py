655a83f24c25ba1076abc5078c4174adb31329ac
juliusw <uimmv@student.kit.edu>
2023-11-16 16:38:38 +0100

    updated requirements

--- features/get_features.py
+++ features/get_features.py
@@ -30,4 +30,4 @@ if __name__ == '__main__':
             break
     results_final_df = pd.concat(results_final)
     results_final_df = results_final_df.reset_index(drop=True)
-    results_final_df.to_csv(os.path.join(os.path.pardir, "data", "processed", "features.csv"))
+    results_final_df.to_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "processed", "features.csv"))
