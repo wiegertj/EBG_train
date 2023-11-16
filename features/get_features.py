f13de7dbfc02b78d8cacf52e028ddc853914e978
juliusw <uimmv@student.kit.edu>
2023-11-16 16:52:25 +0100

    updated requirements

--- features/get_features.py
+++ features/get_features.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         print(f"Finished computation: {counter} / {len(folder_names)}")
 
         features = extractor.extract_features()
-        features.to_csv(os.path.join(os.getcwd()), "features.csv")
+        features.to_csv(os.path.join(os.getcwd(), "features.csv"), index=False)
         results_final.append(features)
 
         current_working_dir = os.getcwd()
