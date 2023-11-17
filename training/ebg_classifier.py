2260a98025c62421e5719c286e753c754e1a3e07
juliusw <uimmv@student.kit.edu>
2023-11-17 10:38:12 +0100

    training dataset

--- training/ebg_classifier.py
+++ training/ebg_classifier.py
@@ -30,58 +30,6 @@ def light_gbm_classifier(threshold, rfe=False, rfe_feature_n=10):
     rfe_feature : int
         number of features for recursive feature elimination 
     """
-    df = df[["dataset", "branchId", "support", "parsimony_boot_support",
-             "parsimony_support",
-             "avg_subst_freq",
-             "length_relative",
-             "length",
-             "avg_rel_rf_boot",
-             "max_subst_freq",
-             "skw_pars_bootsupp_tree",
-             "cv_subst_freq",
-             "bl_ratio",
-             "max_pars_bootsupp_child_w",
-             "sk_subst_freq",
-             "mean_pars_bootsupp_parents",
-             "max_pars_supp_child_w",
-             "std_pars_bootsupp_parents",
-             "min_pars_supp_child",
-             "min_pars_supp_child_w",
-             "rel_num_children",
-             "mean_pars_supp_child_w",
-             "std_pars_bootsupp_child",
-             "mean_clo_sim_ratio",
-             "min_pars_bootsupp_child_w"]]
-
-    """
-        Map columns to verbose feature names
-    """
-    column_name_mapping = {
-        "parsimony_boot_support": "parsimony_bootstrap_support",
-        "parsimony_support": "parsimony_support",
-        "avg_subst_freq": "mean_substitution_frequency",
-        "length_relative": "norm_branch_length",
-        "length": "branch_length",
-        "avg_rel_rf_boot": "mean_norm_rf_distance",
-        "max_subst_freq": "max_substitution_frequency",
-        "skw_pars_bootsupp_tree": "skewness_bootstrap_pars_support_tree",
-        "cv_subst_freq": "cv_substitution_frequency",
-        "bl_ratio": "branch_length_ratio_split",
-        "max_pars_bootsupp_child_w": "max_pars_bootstrap_support_children_w",
-        "sk_subst_freq": "skw_substitution_frequency",
-        "mean_pars_bootsupp_parents": "mean_pars_bootstrap_support_parents",
-        "max_pars_supp_child_w": "max_pars_support_children_weighted",
-        "std_pars_bootsupp_parents": "std_pars_bootstrap_support_parents",
-        "min_pars_supp_child": "min_pars_support_children",
-        "min_pars_supp_child_w": "min_pars_support_children_weighted",
-        "rel_num_children": "number_children_relative",
-        "mean_pars_supp_child_w": "mean_pars_support_children_weighted",
-        "std_pars_bootsupp_child": "std_pars_bootstrap_support_children",
-        "mean_clo_sim_ratio": "mean_closeness_centrality_ratio",
-        "min_pars_bootsupp_child_w": "min_pars_bootstrap_support_children_w"
-    }
-
-    df = df.rename(columns=column_name_mapping)
     df.fillna(-1, inplace=True)
     df.replace([np.inf, -np.inf], -1, inplace=True)
 
