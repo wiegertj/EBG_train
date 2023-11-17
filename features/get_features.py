2f2e9d4ab185ada0251d4b2748d708f25790312d
juliusw <uimmv@student.kit.edu>
2023-11-17 10:15:56 +0100

    training dataset

--- features/get_features.py
+++ features/get_features.py
@@ -38,4 +38,4 @@ if __name__ == '__main__':
 
     results_final_df = pd.concat(results_final)
     results_final_df = results_final_df.reset_index(drop=True)
-    results_final_df.to_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "processed", "features.csv"), index=False)
+    results_final_df.to_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "processed", "features", "features.csv"), index=False)
--- scripts/merge_datasets.py
+++ scripts/merge_datasets.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-features = pd.read_csv( os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "processed", "features.csv"))
+features = pd.read_csv( os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "processed", "features", "features.csv"))
 targets = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "processed", "target", "branch_supports.csv"))
 
 merged_df = features.merge(targets, how="inner", on=["dataset", "branchId"])
