fa6e18399627f17b33ba0428f7f5831785b32626
juliusw <uimmv@student.kit.edu>
2025-01-15 21:26:20 +0100

    add training tabpfn

--- training/ebg_regressor.py
+++ training/ebg_regressor.py
@@ -186,7 +186,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, train_light=False):
         return sum(val_scores) / len(val_scores)
 
     study = optuna.create_study(direction='minimize')
-    study.optimize(objective_median, n_trials=5)
+    study.optimize(objective_median, n_trials=100)
     df = pd.DataFrame({'Value': val_scores_median})
 
     df.to_csv('val_scores.csv', index=False)
@@ -261,7 +261,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, train_light=False):
         return sum(val_scores) / len(val_scores)
 
     study = optuna.create_study(direction='minimize')
-    study.optimize(objective_lower_bound_5, n_trials=5)
+    study.optimize(objective_lower_bound_5, n_trials=100)
 
     best_params_lower_bound = study.best_params
     best_params_lower_bound["objective"] = "quantile"
@@ -319,7 +319,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, train_light=False):
         return sum(val_scores) / len(val_scores)
 
     study = optuna.create_study(direction='minimize')
-    study.optimize(objective_lower_bound_10, n_trials=5)
+    study.optimize(objective_lower_bound_10, n_trials=100)
 
     best_params_lower_bound = study.best_params
     best_params_lower_bound["objective"] = "quantile"
